1fdeca14b9ce307e2b51c3aa9a131466b3a8098d
Alok Menghrajani <alok@squareup.com>
2015-08-08 13:45:06 -0700

    More tests.

--- selfmate/selfmate_test.go
+++ selfmate/selfmate_test.go
@@ -73,3 +73,135 @@ func TestPuzzle3(t *testing.T) {
 		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
 	}
 }
+
+/**
+ * Mikan 1955, selfmate in 3
+ */
+func TestPuzzle4(t *testing.T) {
+	s :=
+		"........\n" +
+			"...N...p\n" +
+			"....R.BQ\n" +
+			".R..bq..\n" +
+			"....k..K\n" +
+			"....P...\n" +
+			"....PP..\n" +
+			".......N\n"
+	board, _ := core.Parse(s, core.White)
+	logger := logger.New(false)
+	selfmate := Find(logger, board, 6)
+	expecting := "g6→h7,h6→h7,e4→f5,f2→f3,f5→g6,e6→e8"
+	if selfmate.String() != expecting {
+		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
+	}
+}
+
+/**
+ * Mikan 1977, selfmate in 3
+ */
+func TestPuzzle5(t *testing.T) {
+	s :=
+		"......R.\n" +
+			"......NR\n" +
+			"........\n" +
+			"........\n" +
+			".....Pk.\n" +
+			"...N.qp.\n" +
+			"......Qp\n" +
+			"...BB..K\n"
+	board, _ := core.Parse(s, core.White)
+	logger := logger.New(false)
+	selfmate := Find(logger, board, 6)
+	expecting := "e2→h2,g2→h2,g4→h4,h5→g5,f3→e2,h7→h5"
+	if selfmate.String() != expecting {
+		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
+	}
+}
+
+/**
+ * Bakcsi 1981, selfmate in 3
+ */
+func TestPuzzle6(t *testing.T) {
+	s :=
+		"...R....\n" +
+			"........\n" +
+			".N..p...\n" +
+			"...b.B..\n" +
+			"..R.r...\n" +
+			"...k....\n" +
+			".P.p.Q..\n" +
+			"...K....\n"
+	board, _ := core.Parse(s, core.White)
+	logger := logger.New(false)
+	selfmate := Find(logger, board, 6)
+	expecting := "d5→b3,b4→b3,e5→d4,f2→d4,e6→e5,c4→b4"
+	if selfmate.String() != expecting {
+		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
+	}
+}
+
+/**
+ * Labai 2007, selfmate in 3
+ */
+func TestPuzzle7(t *testing.T) {
+	s :=
+		"..B.....\n" +
+			"...RN...\n" +
+			".....p..\n" +
+			"........\n" +
+			"....knRK\n" +
+			"....n.B.\n" +
+			"....Q...\n" +
+			"........\n"
+	board, _ := core.Parse(s, core.White)
+	logger := logger.New(false)
+	selfmate := Find(logger, board, 6)
+	expecting := "f6→g5,g4→g5,e5→f5,e2→e3,e4→e5,e7→f5"
+	if selfmate.String() != expecting {
+		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
+	}
+}
+
+/**
+ * Kubbel 1933, selfmate in 3
+ */
+func TestPuzzle8(t *testing.T) {
+	s :=
+		"....k.q.\n" +
+			"p....np.\n" +
+			"....K.B.\n" +
+			"..B.QN.p\n" +
+			"..NR...p\n" +
+			".p......\n" +
+			"bP......\n" +
+			"....b...\n"
+	board, _ := core.Parse(s, core.White)
+	logger := logger.New(false)
+	selfmate := Find(logger, board, 6)
+	expecting := "f7→d6,d5→c4,e8→d8,e6→d5,a2→b1,c4→b6"
+	if selfmate.String() != expecting {
+		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
+	}
+}
+
+/**
+ * Brown 1974, selfmate in 3
+ */
+func TestPuzzle9(t *testing.T) {
+	s :=
+		"........\n" +
+			"........\n" +
+			"........\n" +
+			"........\n" +
+			".....N.p\n" +
+			".......B\n" +
+			"........\n" +
+			"....RKBk\n"
+	board, _ := core.Parse(s, core.White)
+	logger := logger.New(false)
+	selfmate := Find(logger, board, 6)
+	expecting := "h3→g2,f4→e2,h4→h3,h3→g2,h1→h2,g1→f2"
+	if selfmate.String() != expecting {
+		t.Error(fmt.Printf("Expecting: %s, but got %s", expecting, selfmate))
+	}
+}
